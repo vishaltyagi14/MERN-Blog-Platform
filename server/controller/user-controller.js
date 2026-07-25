dfee9b81513f38c440785c70fde482b8f854f21d
Vishal Tyagi <vishal.tyagi14oct@gmail.com>
2026-07-25 10:14:05 +0530

    Working on login api

--- server/controller/user-controller.js
+++ server/controller/user-controller.js
@@ -3,16 +3,16 @@ import bcrypt from "bcrypt"
 
 // Signup API
 
-export const signupUser=async(req,res)=>{
+export const signupUser = async (req, res) => {
     try {
-        const {name,password,username}= req.body;
-        const hashPass=await bcrypt.hash(password,10)
-        const newUser= await User.create({
+        const { name, password, username } = req.body;
+        const hashPass = await bcrypt.hash(password, 10)
+        const newUser = await User.create({
             name,
             username,
             password: hashPass
         })
-// Send to Frontend 
+        // Send to Frontend 
 
         return res.status(200).json({
             success: true,
@@ -20,7 +20,7 @@ export const signupUser=async(req,res)=>{
 
         })
     } catch (error) {
-        console.error("Error: ",error)
+        console.error("Error: ", error)
         return res.status(500).json({
             success: false,
             message: "Error while signup the user"
@@ -29,7 +29,34 @@ export const signupUser=async(req,res)=>{
     }
 }
 
-export const loginUser= async(req,res)=>{
-    let {username,password}= req.body
-    let user
+export const loginUser = async (req, res) => {
+    let { username, password } = req.body
+    let user = await User.findOne({
+        username
+    })
+    if (!user) {
+        return res.status(400).json({
+            success: "false",
+            message: "Username doesn't match"
+        })
+    }
+    try {
+        let match = await bcrypt.compare(password, user.password)
+        if (match) {
+            const accessToken= jwt.sign(user.json(),process.env.ACCESS_TOKEN_KEY)
+            const refreshToken
+        } else {
+            return res.status(400).json({
+                success: "false",
+                message: "username or password is wrong"
+            })
+        }
+    } catch (error) {
+        console.error("Error: ", error)
+        return res.status(500).json({
+            success: false,
+            message: "Error while login the user"
+
+        })
+    }
 }
