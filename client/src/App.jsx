8e7d3a12863da32e6ad8e9d5c22199c97c2ae853
Vishal Tyagi <vishal.tyagi14oct@gmail.com>
2026-07-29 22:56:19 +0530

    header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,12 +4,14 @@ import { Box } from "@mui/material";
 import ContextProvider from "./context/Context";
 import Home from "./components/home/home";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/header/header";
 
 const App = () => {
   return (
     <>
       <ContextProvider>
         <BrowserRouter>
+        <Header/>
           <Routes>
             <Route path="/login" element={<Login />} />
             <Route path="/" element={<Home />} />
