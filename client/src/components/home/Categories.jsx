0382837983e15d95d949ee6c5dab9c7f6f74da26
Vishal Tyagi <vishal.tyagi14oct@gmail.com>
2026-07-31 17:08:18 +0530

    Home

--- client/src/components/home/Categories.jsx
+++ client/src/components/home/Categories.jsx
@@ -1,31 +1,34 @@
-import React from 'react'
-import { Button,Table , TableBody, TableCell, TableHead, TableRow } from '@mui/material'
+import React from "react";
+import {
+  Button,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+} from "@mui/material";
+import { category } from "../../constants/Category";
 
 const Categories = () => {
   return (
     <>
-    <Button>
-        Create Post
-        
-    </Button>
-    <Table>
-            <TableHead>
-                <TableRow>
-                    <TableCell>
-                        All Categories
-                    </TableCell>
-                </TableRow>
-            </TableHead>
-            <TableBody>
-                <TableRow>
-                    <TableCell>
-                        Music
-                    </TableCell>
-                </TableRow>
-            </TableBody>
-        </Table>
+      <Button>Create Post</Button>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>All Categories</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {category.map((c) => (
+            <TableRow key={c.id}>
+              <TableCell>{c.type}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
     </>
-  )
-}
+  );
+};
 
-export default Categories
+export default Categories;
--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,16 +1,21 @@
-import React from 'react'
-import Banner from '../banner/Banner'
-import { Typography } from '@mui/material'
-import Categories from './Categories'
+import React from "react";
+import Banner from "../banner/Banner";
+import { Typography, Grid } from "@mui/material";
+import Categories from "./Categories";
 
 const Home = () => {
   return (
     <>
+      <Banner />
+      <Grid container>
+        <Grid size={{ xs: 12, sm: 2, lg: 2 }}>
+          <Categories />
+        </Grid>
 
-    <Banner/>
-    <Categories/>
+        <Grid size={{ xs: 12, sm: 10, lg: 10 }}>Posts</Grid>
+      </Grid>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
